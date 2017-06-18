ed05d854090040e08db9510803807f5a98c34506
Shock Liang <shockwater@gmail.com>
2017-06-18 16:48:55 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome to chat app",
+        createAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: "Admin",
+        text: "Welcome new user join chat app",
+        createAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('user send new message.', message);
         io.emit('newMessage', {
