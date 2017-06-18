f55dd9191feceea61af070e4e6c64ef58919db7a
Shock Liang <shockwater@gmail.com>
2017-06-18 16:25:08 +0800

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connect to server');
-
-    socket.emit('createMessage', {
-        from: "shock.client@123.com",
-        text: "Hey server!"
-    });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: "shockwater@123.com",
-        text: "Hey~~",
-        createAt: new Date().getTime()
-    });
-
     socket.on('createMessage', (message) => {
         console.log('user send new message.', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
