d3405ec4936de532af7b6f036ef472a74eb9a4f7
Shock Liang <shockwater@gmail.com>
2017-06-20 21:48:53 +0800

    Add form for message and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -10,12 +10,27 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('new message:', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Shock',
-    text: 'Hi'
-}, function(data) {
-    console.log('Got it', data);
-})
+// socket.emit('createMessage', {
+//     from: 'Shock',
+//     text: 'Hi'
+// }, function(data) {
+//     console.log('Got it', data);
+// })
+
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'Shock',
+        text: jQuery('[name=message]').val()
+    }, function() {
+
+    });
+});
 
