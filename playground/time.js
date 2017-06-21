94f9fcf9cabb3e55048ce191a652fa951ba649c0
Shock Liang <shockwater@gmail.com>
2017-06-21 21:56:31 +0800

    Format timestamps of messages

--- playground/time.js
+++ playground/time.js
@@ -2,9 +2,11 @@ const moment = require('moment');
 
 var date = moment();
 // date.add(100, 'year').subtract(9, 'months');
-console.log(date.format('MMM Do,YYYY'));
+// console.log(date.format('MMM Do,YYYY'));
 
 // 10:35 am
-console.log(date.format('H:m a'));
+// console.log(date.format('H:m a'));
 // 06:01 am
-console.log(date.format('HH:mm a'));
+// console.log(date.format('HH:mm a'));
+
+console.log(date.valueOf());
