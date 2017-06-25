a60b9cbcef25f904621937546d62824950028bb1
Shock Liang <shockwater@gmail.com>
2017-06-25 14:17:32 +0800

    Update user list when new user join the chat room

--- server/server.js
+++ server/server.js
@@ -6,11 +6,13 @@ const express = require('express');
 const socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
 const {isRealString} = require('./utils/validation');
+const {Users} = require('./utils/users');
 
+const prot = process.env.PORT;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-const prot = process.env.PORT;
+var users = new Users();
 
 app.use(express.static(publicPath));
 
@@ -23,7 +25,10 @@ io.on('connection', (socket) => {
         }
 
         socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
 
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
         socket.emit('newMessage', generateMessage("Admin", "Welcome to chat app"));
         socket.broadcast.to(params.room).emit('newMessage', generateMessage("Admin", `${params.name} has joined`));
 
@@ -41,7 +46,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('Disconnected from user');
+        var user = users.removeUser(socket.id);
+        if(user) {
+            io.to(user.room).emit('updateUserList',users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin',`${user.name} has left`));
+        }
     });
 });
 
